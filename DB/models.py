639bd70e59d4e85018684910f52313b5348d5283
Phani-Kanala <sriphanisainath@gmail.com>
2018-11-17 11:19:08 -0800

    Modified models.py

--- DB/models.py
+++ DB/models.py
@@ -5,23 +5,24 @@ from django.db import models
 # Create your models here.
 
 
-"""class Enrollments(models.Model):
+class Enrollments(models.Model):
     student_id = models.IntegerField(primary_key=True)
-    course_id = models.IntegerField(null=False)
-    status = models.CharField(max_length=15)"""
+    course_id = models.CharField(null=False,max_length=15)
+    status = models.CharField(max_length=15)
 
 class csvfile(models.Model):
-    req_file = models.FileField(upload_to='documents/')
+    req_file = models.FileField(upload_to='documents')
 
 
 class Marks(models.Model):
     student_id = models.IntegerField(default=0)
-    course_id = models.IntegerField(default=0)
+    course_id = models.CharField(default=0,max_length=15)
+    prof_id = models.IntegerField(default=0)
     marks = models.FloatField(default=0)
     q_name = models.CharField(max_length=15)
 
     class Meta:
-        unique_together = ('student_id', 'course_id','q_name')
+        unique_together = ('student_id', 'course_id','prof_id','q_name')
 
 
 
