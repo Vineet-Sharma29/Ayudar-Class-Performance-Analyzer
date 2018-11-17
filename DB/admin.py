678d777a2f771626a0eff7613cecb5608bb57563
Phani-Kanala <sriphanisainath@gmail.com>
2018-11-17 11:14:01 -0800

    Modified admin.py

--- DB/admin.py
+++ DB/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Marks
 from .models import csvfile
+from .models import Enrollments
+
 
 admin.site.register(csvfile)
 admin.site.register(Marks)
+admin.site.register(Enrollments)
