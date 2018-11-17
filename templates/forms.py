ca0bbf6509279bb93e49f8d0acaf63d4a4addcb4
Phani-Kanala <sriphanisainath@gmail.com>
2018-11-17 11:30:54 -0800

    Removed unnecessary files

--- templates/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import csvfile
-class file_class(forms.ModelForm):
-    #title = forms.CharField(max_length=50)
-    file = forms.FileField()
-    class Meta:
-        model = csvfile
-        fields = ('req_file',)
