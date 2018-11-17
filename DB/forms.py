25f6f8d10f15595b2fcdea6639e99f66e023bc03
Phani-Kanala <sriphanisainath@gmail.com>
2018-11-17 11:18:29 -0800

    Modified forms.py

--- DB/forms.py
+++ DB/forms.py
@@ -1,8 +1,8 @@
-from django import forms
-from .models import csvfile
-class file_class(forms.ModelForm):
-    #title = forms.CharField(max_length=50)
-    file = forms.FileField()
-    class Meta:
-        model = csvfile
-        fields = ('req_file',)
+# from django import forms
+# from .models import csvfile
+# class file_class(forms.ModelForm):
+#     #title = forms.CharField(max_length=50)
+#     req_file = forms.FileField()
+#     class Meta:
+#         model = csvfile
+#         fields = ('req_file',)
