f0c86e9b3d09560e3a4b6f722dc7cd6a80e74e65
Phani-Kanala <sriphanisainath@gmail.com>
2018-11-17 11:19:56 -0800

    Modified views.py

--- DB/views.py
+++ DB/views.py
@@ -10,21 +10,6 @@ from django.core.files.storage import FileSystemStorage
 def uselesspage(request):
     return render(request, 'DB/uselesspage.html')
 
-# def output(request):
-#     if request.method == "POST":
-#         print("checkedhiohohoerhgrepgjergegergogioioijoi")
-#         myform = file_class(request.POST, request.FILES)
-#         if myform.is_valid():
-#             print("checkedasdfghjkl")
-#             tab = csvfile()
-#             tab.req_file = myform.cleaned_data["file"]
-#             tab.save()
-#             #saved = True
-#             #instance = csvfile.objects.create(req_file=request.FILES['file'])
-#             #instance.save()
-#     return HttpResponse("check your database :)")
-
-
 def output(request):
     if request.method == 'POST' and request.FILES['naam']:
         myfile = request.FILES['naam']
@@ -36,29 +21,44 @@ def output(request):
         return some(url)
 
 def some(pat):
-    var = "/home/phani/PycharmProjects/ASE/"
-    csvfile.objects.create(req_file=var+pat)
-    with open(var+pat)as f:
+    #var = "/home/phani/PycharmProjects/ASE/"
+    csvfile.objects.create(req_file=pat)
+    co_id="DSAA"
+    Marks.objects.filter(course_id=co_id).delete()
+    pr_id=2
+    with open(pat)as f:
         f1 = f.readline().split(",")
         f1[len(f1) - 1] = f1[len(f1) - 1].rstrip()
-        # print(f1)
-        f.seek(0, 0)
+        #printreq(f1)
+        f.seek(0,0)
         f_all = f.readlines()
+        return_tuple(f_all)
         f_all[len(f_all) - 1] = f_all[len(f_all) - 1] + '\n'
         for i in range(1, len(f_all)):
             f_all[i] = f_all[i].rstrip()
             f2 = f_all[i].split(',')
             for j in range(0, len(f2) - 1):
                 sid = f2[0]
-                cid = 2
                 marks = f2[j + 1]
                 name = f1[j + 1]
-                some =  Marks.objects.create(student_id=sid, marks=marks, q_name=name, course_id=cid)
-    #printreq(Marks.objects.all())
+
+                some =  Marks.objects.create(student_id=sid, marks=marks, q_name=name, course_id=co_id, prof_id=pr_id)
+    #printreq1(Marks.objects.all())
     #printreq(v)
+    call()
     return HttpResponse("check your database :)")
 
-#def printreq(a):
+def call():
+    print(Marks.objects.filter(q_name="q3"))
+
+def return_tuple(line):
+    req_tuple = ()
+    for n in range(1, len(line)):
+        line[n] = line[n].rstrip()
+        x = tuple(line[n].split(','))
+        req_tuple += (x,)
+    print(req_tuple)
+
     # tup=tuple(a)
     # print(tup(0))
 
