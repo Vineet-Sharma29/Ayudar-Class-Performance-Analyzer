40c97f2f5032e4426a09209dbda207ba4b1f7d4b
Phani-Kanala <sriphanisainath@gmail.com>
2018-11-17 11:17:35 -0800

    Revert "Modified settings.py"
    This reverts commit c005b96d1095f018dfc60c2e46069e9090e20f26.

--- ASE/settings.py
+++ ASE/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'DB',
+    'DB.apps.DbConfig',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_URL = '/home/phani/PycharmProjects/ASE/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
