c121067f6e571567d8b73adb6d682264e41a07d8
Vineet-Sharma29 <vineet.s17@iiits.in>
2018-11-18 20:14:35 +0530

    minor changes in settings.py

--- ase1/settings.py
+++ ase1/settings.py
@@ -80,6 +80,9 @@ WSGI_APPLICATION = 'ase1.wsgi.application'
 DATABASES = {
     'default': {
 
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ase1',
         'USER': 'root',
