accb73207acf11d6f41983063ace9c968eac8728
Vineet-Sharma29 <vineet.s17@iiits.in>
2018-11-25 17:21:36 +0530

    fixed url bugs

--- ase1/settings.py
+++ ase1/settings.py
@@ -79,16 +79,14 @@ WSGI_APPLICATION = 'ase1.wsgi.application'
 DATABASES = {
     'default': {
 
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
-
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ase1',
-        'USER': 'root',
-        'PASSWORD': 'bradison',
-        'PORT': '3306',
-
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'ase1',
+        # 'USER': 'root',
+        # 'PASSWORD': 'bradison',
+        # 'PORT': '3306',
 
     }
 }
