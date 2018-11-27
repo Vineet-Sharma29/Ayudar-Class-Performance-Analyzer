6a795a2caf0a855c86360d6f5991aeec19a04c84
Vineet-Sharma29 <vineet.s17@iiits.in>
2018-11-27 13:47:24 +0530

    modified sidebar

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name="dashboard"
+
 urlpatterns = [
     path('', views.dashboard, name="dashboard"),
     path('needy_list/', views.needy_list, name='needy_list'),
