f3761da25113fde05ad6459b3d4e6fbe668fc4d6
Vineet-Sharma29 <vineet.s17@iiits.in>
2018-12-09 02:50:09 +0530

    deleted unnecessary files

--- aniket/PDF/htmltopdf.py
+++
@@ -1,3 +0,0 @@
-import pdfkit
-
-pdfkit.from_file('list2.html', 'out.pdf')
--- graphs/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Student, Quiz, QuizResult
-
-admin.site.register(Student)
-admin.site.register(Quiz)
-admin.site.register(QuizResult)
